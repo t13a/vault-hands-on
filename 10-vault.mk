a064a56ad5aab6375c2e80f04906b2338f7d6a00
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2023-09-18 19:10:10 +0900

    Small fix

--- 10-vault.mk
+++ 10-vault.mk
@@ -1,5 +1,5 @@
 vault_container_name = $(shell docker compose ps --format=json | jq -r '.[]|select(.Service=="vault")|.Name')
-vault_container_ip_address = $(shell docker inspect --format=json $(vault_container_name) \
+vault_container_ip_address = $(shell docker inspect --format=json '$(vault_container_name)' \
 	| jq -r '.[].NetworkSettings.Networks|to_entries[]|select(.key!="kind").value.IPAddress')
 vault_addr = http://$(vault_container_ip_address):8200
 vault_token = $(shell docker compose exec vault-insecure-setup vault-insecure-setup root-token)
